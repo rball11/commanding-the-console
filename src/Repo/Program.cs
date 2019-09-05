9516852f05e5b7a4605851f899409aa92f3de842
Ryan Ball <rball11@nait.ca>
2019-09-05 11:37:30 -0600

    Closes #1

---
+++ src/Repo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http; //for the http client
+using static System.Console; //allows you to directly call static methods on the Console class, without the class name qualifier
+
+namespace Repo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World");
+        }
+    }
+}
